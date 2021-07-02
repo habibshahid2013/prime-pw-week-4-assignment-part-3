06983d0042ca6d6d994fedb04db6271ec3b6fa1a
Hassan Shahid <habibshahid2013@gmail.com>
2021-07-02 00:05:42 -0500

    Still need some work

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,18 +4,30 @@ console.log('***** Cart Functions *****');
 let basket = []
 console.log('This basket is currently empty', basket);
 
-function addItem(item){
-  basket.push(item);
+function addItem(item){ //this function is adding a local varible "item"
+  basket.push(item); //the local varible is being pushed into the basket array varible
   return true;
 }
 
 console.log(`testing the basket ${basket}`);
 console.log('add some candy to the basket', addItem('candy'));
 console.log(`Checking added ${basket}`, basket);
+console.log('lets add some chicken',addItem('chicken'));
+console.log('lets add some chocolate', addItem('chocolate'));
 
-function listItems(array) {
-  for( let i = 0; i < array.length; i++){
-console.log(`Item ${i + 1}: ${array[i]}`);
+function listItems() {
+  for( let items of basket){
+    console.log('here the items in the basket', items);
   }
 }
-listItems();
+listItems() //testing the list item function.
+
+function empty(items){
+ for(basket.pop of basket);
+  return 'Complete';
+  }
+
+console.log('checking to see deleted ', empty());
+
+console.log('checking basket list', listItems());
+console.log(basket);
