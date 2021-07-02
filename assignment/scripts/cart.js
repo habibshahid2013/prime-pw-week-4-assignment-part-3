c10c3e0a6f8056d61103c3a2ebce144858351fbd
Hassan Shahid <habibshahid2013@gmail.com>
2021-07-02 14:31:27 -0500

    Complated assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,8 +1,18 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const maxItems = 5
 let basket = []
 console.log('This basket is currently empty', basket);
+
+function isFull(){
+  for ( let i = 0; i < basket.length; i++){
+  if (basket[i] <= maxItems){
+      return false;
+    }
+      return true;
+  }
+}
 
 function addItem(item){ //this function is adding a local varible "item"
   basket.push(item); //the local varible is being pushed into the basket array varible
@@ -14,20 +24,22 @@ console.log('add some candy to the basket', addItem('candy'));
 console.log(`Checking added ${basket}`, basket);
 console.log('lets add some chicken',addItem('chicken'));
 console.log('lets add some chocolate', addItem('chocolate'));
+console.log('lets add some tomato',addItem('tomato'));
+console.log('lets add some milk', addItem('milk'));
 
-function listItems() {
+function listItems() {     //creating function to list items in basket 
   for( let items of basket){
     console.log('here the items in the basket', items);
   }
 }
 listItems() //testing the list item function.
 
-function empty(items){
- for(basket.pop of basket);
-  return 'Complete';
+function empty(){
+let items = basket.splice(0, basket.length);
+  return items;
   }
 
 console.log('checking to see deleted ', empty());
 
-console.log('checking basket list', listItems());
 console.log(basket);
+console.log('no room for the items', isFull());
