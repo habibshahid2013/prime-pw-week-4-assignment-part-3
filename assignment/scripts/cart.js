98e4af1d0b82a9cc5b97d7e2601f4017798e5b63
Hassan Shahid <habibshahid2013@gmail.com>
2021-07-04 11:47:42 -0500

    Complated assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,11 +1,11 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-const maxItems = 5
+const maxItems = 5 
 let basket = []
 console.log('This basket is currently empty', basket);
 
-function isFull(){
+function isFull(){  //creating a function to loop through array to check if it full
   for ( let i = 0; i < basket.length; i++){
   if (basket[i] <= maxItems){
       return false;
@@ -27,19 +27,18 @@ console.log('lets add some chocolate', addItem('chocolate'));
 console.log('lets add some tomato',addItem('tomato'));
 console.log('lets add some milk', addItem('milk'));
 
-function listItems() {     //creating function to list items in basket 
+function listItems() {     //creating function to list items in basket
   for( let items of basket){
     console.log('here the items in the basket', items);
   }
 }
 listItems() //testing the list item function.
 
-function empty(){
-let items = basket.splice(0, basket.length);
+function empty(){   //creating a function to empty out the current array
+  let items = basket.splice(0, basket.length);
   return items;
   }
 
-console.log('checking to see deleted ', empty());
-
-console.log(basket);
+console.log('checking to see if items are deleted ', empty());
+console.log('basket is now empty',basket);
 console.log('no room for the items', isFull());
